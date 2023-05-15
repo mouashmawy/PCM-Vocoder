fc20abd7b741dd34a492d21ae9378b9a44bc5add
Moustafa Ashamwy <mouashmawy@gmail.com>
2023-05-15 20:49:01 +0300

    init

---
+++ DEFs.py
@@ -0,0 +1,4 @@
+import enum
+class Quantizers(enum.Enum):
+    MID_RISE = 1
+    MID_TREAD = 2
---
+++ encoder.py
@@ -0,0 +1,36 @@
+from DEFs import *
+
+
+###############-----ENCODING PART----#####################
+
+
+
+
+# The Sampler function, with the required sampling frequency as input.
+def sampler(sampling_frequency:int):
+    pass
+
+def quantizer(quantizer_type:Quantizers, number_of_levels:int, peak_level:int):
+    print(type(quantizer_type))
+    pass
+
+
+def encoder():
+    pass
+
+
+
+
+
+
+
+###############-----MAIN SCRIPT----#####################
+
+def main():
+    quantizer('d',1,1)
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
